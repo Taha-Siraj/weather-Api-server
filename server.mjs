9693a6f4592e41757f80360537607aecaf754059
tahasiraj101 <pikachugaming899@gmail.com>
2025-04-27 11:49:49 -0700

    api server

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(cors())
 
-app.get("/weatherApp/:cityname",(req ,res) => { 
+app.get("/api/weatherApp/:cityname",(req ,res) => { 
     const cityname = req.params.cityname.toLowerCase();
     let  apiRes = {};
     let cities = [
